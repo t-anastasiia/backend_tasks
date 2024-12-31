44c05d30bd7d37057a8845a2234feab4d7a5f4a9
t-anastasiia <nastaytalmazan@gmail.com>
2024-12-31 19:45:03 +1000

    я больше не могу

--- gateway_project/gateway/views.py
+++ gateway_project/gateway/views.py
@@ -1,8 +1,6 @@
-import requests
-from django.http import JsonResponse, HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.http import QueryDict
-import json
+import requests
 
 BASE_SERVER_URL = 'https://backend-tasks-9r0i.onrender.com/'
 
@@ -13,26 +11,27 @@ def proxy_view(request, path=""):
 
     try:
         if request.method == 'GET':
-            response = requests.get(url, headers=headers, params=request.GET)
-        elif request.method == 'HEAD':
-            response = requests.head(url, headers=headers)
+            response = requests.get(url, headers=headers, params=request.GET, stream=True)
         elif request.method == 'POST':
-            if request.content_type == 'application/json':
-                response = requests.post(url, headers=headers, json=json.loads(request.body.decode('utf-8')))
-            else:
-                data = QueryDict(request.body)
-                response = requests.post(url, headers=headers, data=data)
+            response = requests.post(url, headers=headers, data=request.body, stream=True)
         elif request.method == 'PUT':
-            response = requests.put(url, headers=headers, json=json.loads(request.body.decode('utf-8')))
+            response = requests.put(url, headers=headers, data=request.body, stream=True)
         elif request.method == 'DELETE':
-            response = requests.delete(url, headers=headers)
+            response = requests.delete(url, headers=headers, stream=True)
         else:
             return JsonResponse({'error': 'Unsupported HTTP method'}, status=405)
 
-        return HttpResponse(
-            response.content,
+        django_response = HttpResponse(
+            response.raw,  
             status=response.status_code,
-            content_type=response.headers.get('Content-Type', 'text/plain')
+            content_type=response.headers.get('Content-Type')
         )
+        
+        for header, value in response.headers.items():
+            if header.lower() not in ['content-encoding', 'transfer-encoding', 'content-length']:
+                django_response[header] = value
+
+        return django_response
+
     except requests.RequestException as e:
         return JsonResponse({'error': 'Failed to connect to the main server', 'details': str(e)}, status=500)
