bd4871c7b91edfea694171db61d245e2164c8542
t-anastasiia <nastaytalmazan@gmail.com>
2024-12-31 15:41:49 +1000

    home page gateway

--- gateway_project/gateway/views.py
+++ gateway_project/gateway/views.py
@@ -1,18 +1,12 @@
 import requests
-from django.http import JsonResponse, HttpResponse
+from django.http import HttpResponse
 
 FIRST_SERVER_URL = "https://backend-tasks-9r0i.onrender.com"
 
-def proxy_to_first_server(request, endpoint):
-    url = f"{FIRST_SERVER_URL}/{endpoint}"
-    method = request.method
-    headers = request.headers
-    data = request.body
-
-    response = requests.request(method, url, headers=headers, data=data)
-
+def proxy_home(request):
+    response = requests.get(FIRST_SERVER_URL)
     return HttpResponse(
-        response.content, 
-        status=response.status_code, 
+        response.content,
+        status=response.status_code,
         content_type=response.headers.get('Content-Type', 'application/json')
     )
