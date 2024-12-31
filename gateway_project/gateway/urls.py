fc060fda583c17e3ae2611d7d9d8c67e56bf4e1d
t-anastasiia <nastaytalmazan@gmail.com>
2024-12-31 15:55:01 +1000

    proxy_home

--- gateway_project/gateway/urls.py
+++ gateway_project/gateway/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.proxy_home, name='home'),
-    path('proxy/<path:endpoint>/', views.proxy_to_first_server, name='proxy'),
+    path('', views.proxy_home, name='home'),  
+    path('proxy/<path:endpoint>/', views.proxy_to_first_server, name='proxy'),  
 ]
--- gateway_project/gateway/views.py
+++ gateway_project/gateway/views.py
@@ -3,6 +3,14 @@ from django.http import HttpResponse
 
 FIRST_SERVER_URL = "https://backend-tasks-9r0i.onrender.com"
 
+def proxy_home(request):
+    response = requests.get(FIRST_SERVER_URL)
+    return HttpResponse(
+        response.content,
+        status=response.status_code,
+        content_type=response.headers.get('Content-Type', 'text/html')
+    )
+
 def proxy_to_first_server(request, endpoint):
     url = f"{FIRST_SERVER_URL}/{endpoint}"
     method = request.method
